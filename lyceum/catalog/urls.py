8c7e461bdb556e374b83476ec92bb145183254b5
Nikolay <cowboyjack63@gmail.com>
2023-02-17 00:21:08 +0300

    http 200 status

--- lyceum/catalog/urls.py
+++ lyceum/catalog/urls.py
@@ -2,4 +2,4 @@ from django.urls import path
 
 from . import views
 
-urlpatterns = [path("", views.item_list), path("<int:pk>", views.item_detail)]
+urlpatterns = [path("", views.item_list), path("<int:pk>/", views.item_detail)]
