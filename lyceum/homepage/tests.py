d170be846701ac5ca3ae0009c1ddb4bddf0fe87d
Nikolay <cowboyjack63@gmail.com>
2023-02-16 22:51:59 +0300

    homepage ci

--- lyceum/homepage/tests.py
+++ lyceum/homepage/tests.py
@@ -4,5 +4,5 @@ from django.test import TestCase
 
 class StaticURLTests(TestCase):
     def test_homepage_endpoint(self):
-        response = Client().get("/homepage/")
+        response = Client().get("")
         self.assertEqual(response.status_code, 200)
