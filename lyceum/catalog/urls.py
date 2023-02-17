0ddfd3b57bdbac407cc4f90740ff93aed9278f50
Nikolay <cowboyjack63@gmail.com>
2023-02-17 23:36:20 +0300

    small fix to tests

--- lyceum/catalog/urls.py
+++ lyceum/catalog/urls.py
@@ -12,5 +12,5 @@ urlpatterns = [
     path("", views.item_list),
     path("<int:pk>/", views.item_detail),
     path("converter/<int_val:value>/", views.reg_value),
-    re_path(r"^re/(?P<pk>[1-9]{1}[0-9]*)/$", views.re_path_value),
+    re_path(r"^re/(?P<pk>[1-9][0-9]*)/$", views.re_path_value),
 ]
