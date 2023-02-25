7be98001ed63dd1e7547fc83081b4a921b582304
Nikolay <cowboyjack63@gmail.com>
2023-02-25 12:53:41 +0300

    test db

--- lyceum/catalog/migrations/0001_initial.py
+++ lyceum/catalog/migrations/0001_initial.py
@@ -49,7 +49,8 @@ class Migration(migrations.Migration):
                         validators=[
                             django.core.validators.RegexValidator(
                                 re.compile("^[-\\w]+\\Z"),
-                                "Enter a valid “slug” consisting of Unicode letters, numbers, underscores, or hyphens.",
+                                "Enter a valid “slug” consisting of Unicode"
+                                "letters, numbers, underscores, or hyphens.",
                                 "invalid",
                             ),
                             django.core.validators.MaxLengthValidator(200),
